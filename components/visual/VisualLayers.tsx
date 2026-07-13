0aee1762e26601fa136449ab91a0ad44565b8a1f
GrayHatGhost <32582572+GrayHatGhost@users.noreply.github.com>
2026-07-13 20:51:13 +0300

    Add files via upload

--- components/visual/VisualLayers.tsx
+++ components/visual/VisualLayers.tsx
@@ -1,8 +1,5 @@
+import PointerAtmosphere from "./PointerAtmosphere";
+
 export default function VisualLayers() {
-  return (
-    <div className="visual-layers" aria-hidden="true">
-      <div className="grain-layer" />
-      <div className="vignette-layer" />
-    </div>
-  );
+  return <PointerAtmosphere />;
 }
